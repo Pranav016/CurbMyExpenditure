92e56a1b5ce596b393cf96af193751fdc4cb4769
Pranav <pranavmendi@gmail.com>
2021-04-19 05:15:40 +0530

    Add expenses component and Card as a wrapper component

---
+++ src/components/Card.js
@@ -0,0 +1,10 @@
+// This component acts as a wrapper component over the expenses and the expense items and avoids some duplication of css code
+import './Card.css';
+
+function Card(props) {
+	const classes = ' card ' + props.className;
+	return <div className={classes}>{props.children}</div>; // props.children helps to make this a wrapper component
+	// props.children is implicitly sent to every component
+}
+
+export default Card;
