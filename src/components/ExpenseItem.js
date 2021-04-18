a379ca3c83ef876b0aad1a5d50a6186abccacfad
Pranav <pranavmendi@gmail.com>
2021-04-19 03:24:00 +0530

    Add ExpenseDate component and update .eslintrc.json

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,10 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
 	return (
 		<div className='expense-item'>
-			<div>
-				<div>
-					{props.date.toLocaleString('en-US', { month: 'long' })}
-				</div>
-				<div>
-					{props.date.toLocaleString('en-US', { day: '2-digit' })}
-				</div>
-				<div>{props.date.getFullYear()}</div>
-			</div>
+			<ExpenseDate date={props.date} />
 			<div className='expense-item__description'>
 				<h2>{props.title}</h2>
 				<div className='expense-item__price'>${props.amount}</div>
