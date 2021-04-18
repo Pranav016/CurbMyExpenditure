233811efbf68f1e247252666e9a57b3b4f7ccedd
Pranav <pranavmendi@gmail.com>
2021-04-18 22:04:13 +0530

    Style ExpenseItem component and update eslintrc for tab conflict

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ExpenseItem from './components/ExpenseItem';
 function App() {
     return (
         <div>
-            <h1>Let's get started!</h1>
+            <h1>Curb-My-Pocket</h1>
             <ExpenseItem></ExpenseItem>
         </div>
     );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem() {
-    return <h2>Expense Item</h2>;
+	return (
+		<div className='expense-item'>
+			<div>Date</div>
+			<div className='expense-item__description'>
+				<h2>Car isurance</h2>
+				<div className='expense-item__price'>$512.19</div>
+			</div>
+		</div>
+	);
 }
 
 export default ExpenseItem;
