8b0ea0c80506c2cf81e966a1f70eedeeffe3322f
Pranav <pranavmendi@gmail.com>
2021-04-20 04:04:54 +0530

    Add NewExpense component to get user data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Expenses from './components/Expenses/Expenses';
 import expenses from './data/dummy_data.js';
+import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
 	return (
 		<div>
 			<h1>Curb-My-Pocket</h1>
+			<NewExpense />
 			<Expenses items={expenses} />
 		</div>
 	);
